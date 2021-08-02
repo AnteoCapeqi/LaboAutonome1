1e3f3c175ad1073c5a2d09a76060c3fd5ec54f25
capeqianteo@gmail.com <capeqianteo@gmail.com>
2021-08-02 12:51:26 +0200

    Addition de deux Fraction

--- FractionBLL/Fraction.cs
+++ FractionBLL/Fraction.cs
@@ -39,28 +39,28 @@ namespace FractionBLL
             Tuple<int, int, int> SimpleFraction = new Tuple<int, int, int>( 0, 0, 0 );
             if (fraction.numerateur > fraction.Denominateur)
             {
-                decimal tempRes = fraction.numerateur / fraction.denominateur;
+                decimal tempRes = fraction.Numerateur / fraction.Denominateur;
                 int res =decimal.ToInt32(tempRes);
-                fraction.numerateur = fraction.numerateur % fraction.denominateur;
-                SimpleFraction = new Tuple<int, int, int>(res, fraction.numerateur, fraction.denominateur);
-            } else if(fraction.numerateur == fraction.Denominateur)
+                fraction.Numerateur = fraction.Numerateur % fraction.Denominateur;
+                SimpleFraction = new Tuple<int, int, int>(res, fraction.Numerateur, fraction.Denominateur);
+            } else if(fraction.Numerateur == fraction.Denominateur)
             {
-                int res = fraction.numerateur / fraction.denominateur;
+                int res = fraction.Numerateur / fraction.Denominateur;
                 SimpleFraction = new Tuple<int, int, int>(res, 1, 1);
             }
             else
             {
-                SimpleFraction = new Tuple<int, int, int>(0, fraction.numerateur, fraction.denominateur);
+                SimpleFraction = new Tuple<int, int, int>(0, fraction.Numerateur, fraction.Denominateur);
             }
             return SimpleFraction;
         }
         public static Fraction MathSimplification(Fraction fraction) 
         {
-            int PGCD = (int)GCD((ulong)fraction.denominateur,(ulong)fraction.numerateur);
+            int PGCD = (int)GCD((ulong)fraction.Denominateur,(ulong)fraction.Numerateur);
             if (PGCD != 1)
             {
-                fraction.denominateur = fraction.denominateur / PGCD;
-                fraction.numerateur = fraction.numerateur / PGCD;
+                fraction.Denominateur = fraction.Denominateur / PGCD;
+                fraction.Numerateur = fraction.Numerateur / PGCD;
             }
             return fraction;
         }
@@ -75,6 +75,35 @@ namespace FractionBLL
             }
 
             return a == 0 ? b : a;
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b) 
+        {
+            Fraction res = new Fraction(1,1);
+            Tuple < Fraction, Fraction, Fraction > temp = DenominateurCommun(a, b, res);
+            a = temp.Item1;
+            b = temp.Item2;
+            res = temp.Item3;
+            res.Numerateur = a.Numerateur + b.Numerateur;
+            res = Fraction.MathSimplification(res);
+            return res;
+        }
+        private static Tuple<Fraction, Fraction, Fraction> DenominateurCommun(Fraction a, Fraction b, Fraction res) 
+        {
+            if (a.Denominateur != b.Denominateur)
+            {
+                int calc = a.Denominateur * b.Denominateur;
+                a.Numerateur *= b.Denominateur;
+                b.Numerateur *= a.Denominateur;
+                a.Denominateur = calc;
+                b.Denominateur = calc;
+                res.Denominateur = calc;
+            }
+            else
+            {
+                res.Denominateur = a.Denominateur;
+            }
+            return new Tuple<Fraction, Fraction, Fraction>(a,b,res);
         }
 
     }
---
+++ TestFraction/FractionOperationTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FractionBLL;
+using System;
+
+namespace TestFraction
+{
+    [TestClass]
+    public class FractionOperationTest
+    {
+        [DataRow(new int[2] { 1, 2 }, new int[4] { 1, 1, 1 , 1 }, DisplayName = "1/1 + 1/1 = 2/1")]
+        [DataRow(new int[2] { 1, 1 }, new int[4] { 5, 4 , 5 , 1 }, DisplayName = "4/5 + 1/5 = 1/1")]
+        [DataRow(new int[2] { 15, 23 }, new int[4] { 3, 4, 5, 1 }, DisplayName = "4/3 + 1/5 = 23/15")]
+        [TestMethod]
+        public void AdditionFractionObject(int[] ExpectedValue, int[] givenvalue)
+        {
+            Fraction fraction1 = new Fraction(givenvalue[0], givenvalue[1]);
+            Fraction fraction2 = new Fraction(givenvalue[2], givenvalue[3]);
+            Fraction res = fraction1 + fraction2;
+            Assert.IsTrue(res.Denominateur == ExpectedValue[0] && res.Numerateur == ExpectedValue[1],
+                res.Numerateur.ToString() + "/" + res.Denominateur.ToString());
+        }
+        
+    }
+
+}
